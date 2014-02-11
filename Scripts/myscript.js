dfe52d4a004f94903b5b3227b7398710e093fd84
bcsf10a054 <bcsf10a054@pucit.edu.pk>
2014-02-11 10:12:15 +0500

    hogaya project

---
+++ Scripts/myscript.js
@@ -0,0 +1,24 @@
+﻿
+$(document).ready(function () {
+
+
+
+    $('#b1').click(function () {
+
+        var userName = $('#username').val();
+
+       // alert("hell");
+        $.getJSON("/home/CheckUserName?username=" + userName, function (data) {
+
+            if (!data) {
+                alert("User already exists in database");
+
+            }
+            else {
+                alert("User Name is available");
+            }
+        });
+
+    });
+
+});
